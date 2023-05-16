46f40b64f910e9629e337669b6ef7bea9279d966
Nikita Pleshakov <alestarprince@yandex.com>
2023-05-16 07:50:34 +0300

    new commit

--- Navigation/InfoViewController.swift
+++ Navigation/InfoViewController.swift
@@ -8,22 +8,66 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
+    // MARK: - private properties
+    
+    private lazy var firstButton: UIButton = { // настройка кнопки внутри инициализатора
+        let button = UIButton() // создание экземпляра класса
+        button.translatesAutoresizingMaskIntoConstraints = false // отключить маску перед настройкой constraints
+        button.setTitle("Показать ALERT!", for: .normal)
+        button.backgroundColor = .blue // настройка цвета кнопки
+        button.layer.cornerRadius = 5 // закругление краев кнопки
+        button.addTarget(self, action: #selector(didTabFirstButton), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setupView()
+        self.setupConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupView() {
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.firstButton)
     }
-    */
-
+    
+    @objc
+    private func didTabFirstButton() {
+        let alert = UIAlertController(
+            title: "Внимание!",
+            message: "Это мой первый Alert!",
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(
+            title: "Ok",
+            style: .default
+        ) { action in
+            guard let title = action.title else {
+                return
+            }
+            print(title)
+        }
+        let cancelAction = UIAlertAction(
+            title: "Отмена",
+            style: .default
+        ) { action in
+            if let text = action.title {
+                print(text)
+            }
+        }
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            self.firstButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            self.firstButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 10),
+            self.firstButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10),
+            self.firstButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
 }
--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class PostViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var post: Post
+    
+    init(post: Post) {
+        self.post = post
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupNavigationBar()
+    }
 
+    private func setupView() {
+        view.backgroundColor = .systemYellow
+        self.navigationItem.title = self.post.title
+    }
+    
+    private func setupNavigationBar() {
+        let barButtonItem = UIBarButtonItem( // создание кнопки
+            image: UIImage(systemName: "info.circle"), // установка картинки, своя картинка named: ""
+            style: .done, // стиль
+            target: self, // где искать функцию
+            action: #selector(didTabBarButtonItem) // вызов функции по имени
+        )
+        self.navigationItem.rightBarButtonItem = barButtonItem
+    }
+    @objc
+    private func didTabBarButtonItem() {
+        let infoViewController = InfoViewController()
+        self.present(infoViewController, animated: true)
+    }
 }
